07322d9670b4c86845ee67e7cd44cb5b3287cca0
Kohina Pandey <pandeykohina@gmail.com>
2026-07-17 17:59:03 +0530

    Initial setup

---
+++ backend/src/main/java/com/droneos/DroneosApplication.java
@@ -0,0 +1,13 @@
+package com.droneos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneosApplication.class, args);
+	}
+
+}
