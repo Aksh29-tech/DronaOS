c186ed342fcce7d251f90e7b5d3593fd30e01012
Akshat Nigam <nigamakshat29jan2000@gmail.com>
2026-07-17 18:06:06 +0530

    P1 - updated module and main class name

--- backend/src/main/java/com/droneos/DronaOSApplication.java
+++ backend/src/main/java/com/droneos/DronaOSApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DroneosApplication {
+public class DronaOSApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DroneosApplication.class, args);
+		SpringApplication.run(DronaOSApplication.class, args);
 	}
 
 }
